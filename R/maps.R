7c1b0d642be3a47e3ecd6ce6a2f4cddee67bdda1
Karthik Ram <karthik.ram@gmail.com>
2014-04-16 14:41:41 -0700

    Adding placeholders for some files from a while back
    Will likely clean these out.

---
+++ R/maps.R
@@ -0,0 +1,5 @@
+http://www.iobis.org/geoserver/OBIS/wms?service=WMS&version=1.1.0&request=GetMap&layers=OBIS:country
+&styles=country&bbox=-180.0,-90.0,180.0,90.0&width=660&height=330
+&srs=EPSG:4326&format=image/jpeg
+
+
